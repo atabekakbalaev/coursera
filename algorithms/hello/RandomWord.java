b80004e17a707df5b6d6603e5181852b4bdde9fb
Atabek Akbalaev <akbalaevatabek@gmail.com>
2021-12-23 17:58:32 +0600

    added week01 part01 project

--- algorithms/hello/RandomWord.java
+++ algorithms/hello/RandomWord.java
@@ -10,14 +10,13 @@ import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomWord {
     public static void main(String[] args) {
-        String champion = null;
+        String champion = StdIn.readString();
         String current;
-        boolean currentProbability = false;
+        int i = 1;
 
         while (!StdIn.isEmpty()) {
             current = StdIn.readString();
-            currentProbability = StdRandom.bernoulli(0.5);
-            if (currentProbability) {
+            if (StdRandom.bernoulli(1.0 / (double) i++)) {
                 champion = current;
             }
         }
